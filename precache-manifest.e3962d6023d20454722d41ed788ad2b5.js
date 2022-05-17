faca0882c215799fa6b4c02cf4f738f58b284918
MariStakhovskaya <maryia.jegorova@gmail.com>
2022-05-17 21:00:19 +0300

    Updates

---
+++ precache-manifest.e3962d6023d20454722d41ed788ad2b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d131cc3f7df9f501270",
+    "url": "/questionCards/static/css/main.59851f9e.chunk.css"
+  },
+  {
+    "revision": "3d131cc3f7df9f501270",
+    "url": "/questionCards/static/js/main.fa2dc762.chunk.js"
+  },
+  {
+    "revision": "aab896a92949507058c3",
+    "url": "/questionCards/static/js/runtime~main.c1b778cd.js"
+  },
+  {
+    "revision": "32ef6542c5338783a662",
+    "url": "/questionCards/static/js/2.e5d68ee3.chunk.js"
+  },
+  {
+    "revision": "b61d7e105384ced4ef5b",
+    "url": "/questionCards/static/js/3.66188ad7.chunk.js"
+  },
+  {
+    "revision": "6cf03e7b81b2ff0c85101cb50ce4fdf4",
+    "url": "/questionCards/static/media/Ellipsis.6cf03e7b.gif"
+  },
+  {
+    "revision": "1340f194fdb7869f41d33d69911549e6",
+    "url": "/questionCards/static/media/404.1340f194.png"
+  },
+  {
+    "revision": "068caec9d2269e53b08b83318c2abb18",
+    "url": "/questionCards/static/media/4698238.068caec9.png"
+  },
+  {
+    "revision": "33d1f3ba8932e0cb8459a8d86541618e",
+    "url": "/questionCards/index.html"
+  }
+];
